1c6f67a513c60efe577c3bb26e15e41f0551c337
Rickyflame <kimumuricky@gmail.com>
2023-01-23 22:24:11 +0300

    task 0

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,13 @@
+export function taskFirst() {
+    const task = 'I prefer const when I can.';
+    return task;
+}
+
+export function getLast() {
+    return ' is okay';
+}
+
+export function tastNext(){
+    let combination = 'But sometimes let';
+    combination += getLast();
+}
