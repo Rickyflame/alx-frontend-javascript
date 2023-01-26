c5c107dd73aee76fba84902f1602ed003dc92e6f
Rickyflame <kimumuricky@gmail.com>
2023-01-26 22:50:57 +0300

    task 1

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,6 @@
+import ClassRoom from './0-classroom.js';
+
+export default function initializeRooms() {
+  const rray = [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+    return rray;
+}
