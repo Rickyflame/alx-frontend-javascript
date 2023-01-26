781904be4186a2466233ed9568afbc44b34797e1
Rickyflame <kimumuricky@gmail.com>
2023-01-26 09:44:31 +0300

    Task 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,10 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-	  	const task = true;
-	  	const task2 = false;
+    // eslint-disable-next-line
+		const task = true;
+    // eslint-disable-next-line
+		const task2 = false;
   }
 
   return [task, task2];
