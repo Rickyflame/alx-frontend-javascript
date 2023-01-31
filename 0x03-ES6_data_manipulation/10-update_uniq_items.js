d68d6ae408f30f3b4a30a42b081d7ae4299861fb
Rickyflame <kimumuricky@gmail.com>
2023-01-31 23:29:15 +0300

    Task 10

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -2,7 +2,7 @@ export default function updateUniqueItems(map) {
   if (typeof map !== 'object') {
     throw new Error ("Cannot process");
   }
-  
+
   Object.keys(map).forEach(function(key) {
     if(map[key] === 1) {
       map[key] = 100;
