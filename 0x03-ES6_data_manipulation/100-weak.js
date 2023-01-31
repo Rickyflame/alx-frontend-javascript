b7637b5fde3ec31596f6f1cc1f49d868a65eab1a
Rickyflame <kimumuricky@gmail.com>
2023-01-31 23:43:42 +0300

    task 11

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,11 @@
+
+export const weakMap = new WeakMap();
+let times = 1;
+
+export function queryAPI(endPoint) {
+  weakMap.set(endPoint, times);
+  times += 1;
+  const countQuery = weakMap.get(endPoint);
+  if (countQuery >= 5) {
+    throw new Error('Endpoint load is high');
+  }
